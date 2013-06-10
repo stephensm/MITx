6d3e7a79076fb03d1163431921f4128cd956dd22
OpenShift guest <marissas@mit.edu>
2013-06-10 14:50:15 -0400

    afternoon session 1

--- day1.js
+++ day1.js
@@ -1,7 +1,86 @@
-function calculate(){
-    var input=$('#text1:first');
-    var val=input.val();
-    var output=$('#text1_out:first');
-    output.text(val);
-    console.log(val);
+function calculate(text){
+    var pattern=/\d+|\+|\-|\*|\/|\(|\)/g;
+    var tokens=text.match(pattern);
+    try {
+
+        var val = evaluate(tokens);
+        if (tokens.length>0){
+            throw "ill-formed expression";
+            }
+        return String(val);
+    }
+    catch (err)
+    {
+
+       return err;   // error message will be printed as the answer
+
+    }
+    return JSON.stringify(tokens);
 }
+function read_operand(tokens) {
+    var num = tokens[0];
+    tokens.shift();
+    var result=parseInt(num); 
+    if (isNaN(result))
+    {
+        throw "not a number";
+    }
+    else
+    {
+        return result;
+    }
+
+}
+
+
+function evaluate (tokens) {
+   if (tokens.length===0){
+       throw "missing operand";
+   }
+   
+   else{
+       var value=read_operand(tokens);
+   } 
+   while (tokens.length>0) {
+        var operator = tokens[0];
+        tokens.shift();
+        if (operator !="+" && operator !="-" && operator !="*" && operator !="/")  
+        {
+           throw "unrecognized operator"; 
+        }
+        if (tokens.length===0){
+                    throw "missing operand";
+                }
+        var temp = read_operand(tokens);
+        if(operator=="+"){
+            value=value+temp;
+        }
+        else if(operator=="-"){
+            value=value-temp;
+        }
+        else if(operator=="*"){
+            value=value*temp;
+        }
+        else if(operator=="/"){
+            value=value/temp;
+        }
+          // perform requested operation
+   }
+   return value;
+}
+function setup_calc(div){
+    var input=$('<input></input>',{type: "text", size: 50});
+    var output=$('<div></div>');
+    var button=$('<button>Calculate</button>');
+    button.bind("click",function(){
+        output.text(String(calculate(input.val())));
+        
+    });
+    $(div).append(input,button,output);
+}
+$(document).ready(function(){
+ $('.calculator').each(function(){
+     //'this' refers to the <div> of class calculator
+     setup_calc(this);
+ });   
+});
