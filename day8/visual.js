fec62ba54d68baf4da388de4fec771ce4114f1a2
stephensm <marissastephens@dhcp-18-111-7-173.dyn.mit.edu>
2013-06-19 11:00:41 -0400

    veritical bar

--- day8/visual.js
+++ day8/visual.js
@@ -1,16 +1,22 @@
 var data = [0,4,8,8,15,16,23,42]
 
-var ch=140;
-yscale=d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0,ch]);
-xscale=d3.scale.linear().domain([0,d3.max(data)]).range(["0%","100%"]);
+var ch=300;
+var cw=300;
+xscale=d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0,cw]);
+yscale=d3.scale.linear().domain([0,d3.max(data)]).range([0,ch]);
 
-var chart=d3.select(".chart-container").append("svg").attr("class","chart");
-chart.selectAll("rect").data(data).enter().append("rect").attr("y",function(d,i){return yscale(i);}).attr("width",xscale).attr("height",20);
+var chart=d3.select(".chart-container").append("svg").attr("class","chart")
+.attr("height",ch).attr("width",cw);
+
+chart.selectAll("rect").data(data).enter().append("rect")
+.attr("x",function(d,i){return xscale(i);})
+.attr("y",function(d){return ch-yscale(d);})
+.attr("width",xscale.rangeBand()).attr("height",yscale);
 
 chart.selectAll("text").data(data).enter().append("text")
 .text(function(d){return d;})
-.attr("y",function(d,i){return yscale(i)+yscale.rangeBand()/2;})
-.attr("x",xscale)
-.attr("dy","0.35em")
-.attr("dx",-3)
-.attr("text-anchor","end");
+.attr("x",function(d,i){return xscale(i)+xscale.rangeBand()/2;})
+.attr("y",function(d){return ch-yscale(d)+3;})
+.attr("dy","0.7em")
+.attr("text-anchor","middle");
+
