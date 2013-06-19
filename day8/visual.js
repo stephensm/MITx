67bb84d1a91a81c6b62df90a5fa71677fdaef8d4
stephensm <marissastephens@dhcp-18-111-7-173.dyn.mit.edu>
2013-06-19 10:34:29 -0400

    bar chart with svg

--- day8/visual.js
+++ day8/visual.js
@@ -1,12 +1,16 @@
 var data = [0,4,8,8,15,16,23,42]
-//var chart=$("<div></div>").addClass("chart")
-//$(".chart-container").append(chart);
 
-//data.forEach(function(d){chart.append(d)});
-//data.forEach(function(d){chart.append($("<div></div>").css("width",d*10 +"px").text(d))});
-
+var ch=140;
+yscale=d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0,ch]);
 xscale=d3.scale.linear().domain([0,d3.max(data)]).range(["0%","100%"]);
 
-var chart=d3.select(".chart-container").append("div").attr("class","chart");
-chart.selectAll("div").data(data).enter().append("div").style("width",xscale)
-.text(function(d){return d});
+var chart=d3.select(".chart-container").append("svg").attr("class","chart");
+chart.selectAll("rect").data(data).enter().append("rect").attr("y",function(d,i){return yscale(i);}).attr("width",xscale).attr("height",20);
+
+chart.selectAll("text").data(data).enter().append("text")
+.text(function(d){return d;})
+.attr("y",function(d,i){return yscale(i)+yscale.rangeBand()/2;})
+.attr("x",xscale)
+.attr("dy","0.35em")
+.attr("dx",-3)
+.attr("text-anchor","end");
